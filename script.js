a667d0d3f1434ebd5179c78274a58212bf50eee8
Benicio <benicio.daniel@gmail.com>
2019-12-17 20:03:59 -0300

    addEventListener mouseleave

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var button = document.getElementsByTagName("button")[0];
+
 button.addEventListener("click", function(){
-    console.log("AHHHH!");
-    
+    console.log("Cliquei!!");
+});
+
+button.addEventListener("mouseleave", function () {
+    console.log("Mouse saiu!!");
 });
